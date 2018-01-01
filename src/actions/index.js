986146289ed5b9b4c2a76632013645f9d5da824a
bobby569 <zhan2175@purdue.edu>
2018-01-01 09:59:05 -0500

    pkg upgrade, webpack config

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
+import { WEATHER_KEY } from '../config/keys';
+import { FETCH_WEATHER } from './types';
 
 const ROOT_URL = 'http://api.openweathermap.org/data/2.5/forecast';
-const API_KEY = 'ce934e3d6cca78c63a1469a65cbf577f';
-const QUERY_URL = `${ROOT_URL}?appid=${API_KEY}`;
-
-export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
-	const url = `${QUERY_URL}&q=${city},us`;
-	const request = axios.get(url);
+	const request = axios.get(`${ROOT_URL}?appid=${WEATHER_KEY}&q=${city},us`);
 
 	return {
 		type: FETCH_WEATHER,
