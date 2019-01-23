c8d3d8ad8573a221dc4f582bca89a9a9906e5551
bobby569 <bobby.yczh@gmail.com>
2019-01-22 22:44:28 -0500

    update

--- webpack.config.js
+++ webpack.config.js
@@ -1,76 +1,83 @@
-const path = require('path');
-const webpack = require('webpack');
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const HTMLWebapckPlugin = require('html-webpack-plugin');
+const path = require("path");
+const webpack = require("webpack");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const HTMLWebapckPlugin = require("html-webpack-plugin");
 
 module.exports = {
-	entry: {
-		bundle: './src/index.js'
-	},
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		publicPath: '/',
-		filename: '[name].js'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				loader: 'babel-loader',
-				exclude: /node_modules/
-			},
-			{
-				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: 'css-loader'
-				})
-			},
-			{
-				test: /\.(jpe?g|png|gif|svg)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: { limit: 40000 }
-					},
-					'image-webpack-loader'
-				]
-			}
-		]
-	},
-	plugins: [
-		new ExtractTextPlugin('style.css'),
-		new HTMLWebapckPlugin({
-			template: './index.html'
-		})
-	],
-	resolve: {
-		extensions: ['.js', '.jsx']
-	},
-	devServer: {
-		historyApiFallback: true,
-		noInfo: true,
-		overlay: true
-	}
+  entry: {
+    bundle: "./src/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    publicPath: "/",
+    filename: "[name].js"
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ExtractTextPlugin.extract({
+          fallback: "style-loader",
+          use: "css-loader"
+        })
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: { limit: 40000 }
+          },
+          "image-webpack-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin("style.css"),
+    new HTMLWebapckPlugin({
+      template: "./index.html"
+    })
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    historyApiFallback: true,
+    noInfo: true,
+    overlay: true
+  }
 };
 
-if (process.env.NODE_ENV === 'production') {
-	module.exports.devtool = '#source-map';
-	module.exports.output.publicPath = './';
-	module.exports.plugins = (module.exports.plugins || []).concat([
-		new webpack.DefinePlugin({
-			'process.env': {
-				NODE_ENV: '"production"'
-			}
-		}),
-		new webpack.optimize.UglifyJsPlugin({
-			sourceMap: true,
-			compress: {
-				warnings: false
-			}
-		}),
-		new webpack.LoaderOptionsPlugin({
-			minimize: true
-		})
-	]);
+if (process.env.NODE_ENV === "production") {
+  module.exports.mode = "production";
+  module.exports.devtool = "#source-map";
+  module.exports.output.publicPath = "./";
+  module.exports.plugins = (module.exports.plugins || []).concat([
+    new webpack.DefinePlugin({
+      "process.env": {
+        NODE_ENV: '"production"'
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      sourceMap: true,
+      compress: {
+        warnings: false
+      }
+    }),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true
+    })
+  ]);
 }
